968483ab0912f4c39d7b2e22a6e573f7b978ccc6
zachDiaks <zachryandax@gmail.com>
2025-02-28 21:22:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Benford's Law with MathWorks Documentation
+Let's see if MathWorks doc pages are compliant with Benford's Law!
---
+++ fetchURLs.py
@@ -0,0 +1,37 @@
+from playwright.async_api import async_playwright, Playwright
+import asyncio
+
+async def fetchAllURLS(playwright: Playwright, mainPage):
+    # Load up main page, wait for it to load
+    webkit = playwright.webkit
+    browser = await webkit.launch()
+    context = await browser.new_context()
+    page = await context.new_page()
+    page.on("load", lambda: print("Page loaded!"))
+    await page.goto(mainPage)
+
+    # Fetch all links
+    links = await page.locator("a").all()
+    urls = [await link.get_attribute("href") for link in links if await link.get_attribute("href")]
+    
+    # Find the valid URLs
+    print(urls)
+    refPages = [extractBefore(mainPage, "referencelist") + url for url in urls if "ref/" in url]
+
+    # Close browser
+    await browser.close()
+
+    return refPages
+
+def extractBefore(str, targetStr):
+    idx = str.find(targetStr)
+    return str[:idx]
+
+async def main():
+    async with async_playwright() as playwright:
+        return await fetchAllURLS(playwright, "https://www.mathworks.com/help/fusion/referencelist.html?type=function")
+#------------------------------------------------------------------------------------------------------------------------
+refPages = asyncio.run(main())
+with open('refPages.txt', 'w') as f:
+    for page in refPages:
+        f.write(f"{page}\n")
